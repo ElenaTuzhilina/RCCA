7a65dc18ef6107068a2434d147da78c0be0f76af
Elena Tuzhilina <elenatuz@stanford.edu>
2020-02-22 11:45:10 -0800

    Added IMR90 cells contact matrices to the data

--- R/data.R
+++ R/data.R
@@ -1,7 +1,23 @@
-#'Example of a contact matrix
+#' Example of a contact matrix
 #' 
 #' usage data(C)
 #' 
 #' @format A 20x20 symmetric nonnegative integer-valued matrix.
 #'
 "C"
+
+#'Hi-C data for IMR90 cells for chromosome 20 and resolution 100kb obtained from the Gene Expression Omnibus
+#' 
+#' usage data(IMR90_100kb_chr20)
+#' 
+#' @format A 625x625 symmetric nonnegative integer-valued matrix.
+#'
+"IMR90_100kb_chr20"
+
+#'Hi-C data for IMR90 cells for chromosome 21 and resolution 100kb obtained from the Gene Expression Omnibus
+#' 
+#' usage data(IMR90_100kb_chr21)
+#' 
+#' @format A 470x470 symmetric nonnegative integer-valued matrix.
+#'
+"IMR90_100kb_chr21"
