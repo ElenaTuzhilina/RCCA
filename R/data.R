1264d25dd8a90941d0f4ebc6e9279e6a760b3067
Elena Tuzhilina <elenatuz@stanford.edu>
2020-02-23 00:58:38 -0800

    added HiC data

--- R/data.R
+++ R/data.R
@@ -6,7 +6,39 @@
 #'
 "C"
 
-#'Hi-C data for IMR90 cells for chromosome 20 and resolution 100kb obtained from the Gene Expression Omnibus
+#' Hi-C data for IMR90 cells for chromosomes 20 and resolutions 25kb obtained from the Gene Expression Omnibus
+#' 
+#' usage data(IMR90_25kb_chr20)
+#' 
+#' @format A 2498x2498 symmetric nonnegative integer-valued matrix.
+#'
+"IMR90_25kb_chr20"
+
+#' Hi-C data for IMR90 cells for chromosomes 21 and resolutions 25kb obtained from the Gene Expression Omnibus
+#' 
+#' usage data(IMR90_25kb_chr21)
+#' 
+#' @format A 1878x1878 symmetric nonnegative integer-valued matrix.
+#'
+"IMR90_25kb_chr21"
+
+#' Hi-C data for IMR90 cells for chromosomes 20 and resolutions 50kb obtained from the Gene Expression Omnibus
+#' 
+#' usage data(IMR90_50kb_chr20)
+#' 
+#' @format A 1249x1249 symmetric nonnegative integer-valued matrix.
+#'
+"IMR90_50kb_chr20"
+
+#' Hi-C data for IMR90 cells for chromosomes 21 and resolutions 50kb obtained from the Gene Expression Omnibus
+#' 
+#' usage data(IMR90_50kb_chr21)
+#' 
+#' @format A 939x939 symmetric nonnegative integer-valued matrix.
+#'
+"IMR90_50kb_chr21"
+
+#' Hi-C data for IMR90 cells for chromosomes 20 and resolutions 100kb obtained from the Gene Expression Omnibus
 #' 
 #' usage data(IMR90_100kb_chr20)
 #' 
@@ -14,7 +46,7 @@
 #'
 "IMR90_100kb_chr20"
 
-#'Hi-C data for IMR90 cells for chromosome 21 and resolution 100kb obtained from the Gene Expression Omnibus
+#' Hi-C data for IMR90 cells for chromosomes 21 and resolutions 100kb obtained from the Gene Expression Omnibus
 #' 
 #' usage data(IMR90_100kb_chr21)
 #' 
